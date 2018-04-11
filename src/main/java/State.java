cdb820da3b4f3be89bf975bd6d87ddc23ca802a1
adinowi <adinowi@gmail.com>
2018-04-11 10:43:43 +0200

    create state class

---
+++ src/main/java/State.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class State {
+    private String name;
+    private ArrayList<Product> productList;
+
+    public State(String name) {
+        this.name = name;
+        productList = new ArrayList<Product>();
+    }
+
+    public void addProduct(Product product){
+        productList.add(product);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+}
