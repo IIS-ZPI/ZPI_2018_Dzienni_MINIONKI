fb8bea6654cbf222fa77b879f90d1467b9bfa802
Rafikus <Rafikus95@gmail.com>
2018-05-10 23:05:12 +0200

    Zmienione UI, lekki refaktor kodu
    Dodałem między innymi funkcje obliczania podatku w kalkulatorze.
    Oraz wypisywanie w UI obliczonych cen dla każdego wpisanego stanu do JSON

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator implements ICalculator {
         String category = product.getCategory();
         String stateName = state.getName();
         //json gets a tax for proper state and category
-        String jsonFilePath = "stany.json";
+        String jsonFilePath = "jsonFile.json";
         FileReader fileReader = new FileReader(jsonFilePath);
         JSONParser jsonParser = new JSONParser();
 
@@ -22,7 +22,6 @@ public class Calculator implements ICalculator {
         JSONObject jsonObject = (JSONObject) jsonParser.parse(fileReader);
 
         JSONArray states = (JSONArray) jsonObject.get("States");
-
         for (Object st : states) {
             JSONObject sta = (JSONObject) st;
             String stName = (String) sta.get("Name");
@@ -39,6 +38,39 @@ public class Calculator implements ICalculator {
             }
         }
 
+        return -1;
+    }
+
+    @Override
+    public double calculateTax(State state, Product product) throws IOException, ParseException {
+
+        double calculatedPrice = 0;
+        double price = product.getPrice();
+        String category = product.getCategory();
+        String stateName = state.getName();
+        //json gets a tax for proper state and category
+        String jsonFilePath = "jsonFile.json";
+        FileReader fileReader = new FileReader(jsonFilePath);
+        JSONParser jsonParser = new JSONParser();
+
+
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(fileReader);
+
+        JSONArray states = (JSONArray) jsonObject.get("States");
+        for (Object st : states) {
+            JSONObject sta = (JSONObject) st;
+            String stName = (String) sta.get("Name");
+            if (stName.equals(state.getName())) {
+                JSONArray categories = (JSONArray) sta.get("Categories");
+                for (Object categoryObject : categories
+                        ) {
+                    JSONObject cat = (JSONObject) categoryObject;
+                    if(category.equals(cat.get("Name"))) {
+                        return ((double) cat.get("Tax") / 100) * price;
+                    }
+                }
+            }
+        }
         return -1;
     }
 }
--- src/main/java/State.java
+++ src/main/java/State.java
@@ -1,24 +1,36 @@
 import java.util.ArrayList;
 
 public class State {
+
     private String name;
-    private ArrayList<Product> productList;
+    private double price;
+    private double tax;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
 
     public State(String name) {
         this.name = name;
-        //productList = new ArrayList<Product>(); // i ve commented this
-    }
-
-    public void addProduct(Product product){
-        productList.add(product);
     }
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Product> getProductList() {
-        return productList;
-    }
+
+
 
 }
