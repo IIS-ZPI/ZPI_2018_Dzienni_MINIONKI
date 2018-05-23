a34d00954907df85ebe2ccb10130dbcb81b88b11
Mrucznik <mrucznix@gmail.com>
2018-05-23 11:48:40 +0200

    JsonIO - constructor tests done

--- src/test/java/JsonIOTest.java
+++ src/test/java/JsonIOTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class JsonIOTest {
 
+    @Test
+    public void constructorTest() {
+        JsonIO jsonIO = new JsonIO("Test json");
+        assertEquals("Test json", jsonIO.name);
+        assertNotEquals(null, jsonIO.states);
+    }
 }
